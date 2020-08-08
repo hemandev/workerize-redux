e618d14091d4c5648c68a77b110159c6decfa0d3
hemanditwiz <hemanditwiz@gmail.com>
2020-08-09 00:26:29 +0530

    Remove lib folder

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Reducer } from 'redux';
-import { ReduxWorker } from './reduxWorker';
-export { applyWorkerMiddleWare } from './applyWorkerMiddleware';
-export declare const createWorker: (reducer: Reducer<any, import("redux").AnyAction>) => ReduxWorker;
--- lib/reduxWorker.d.ts
+++
@@ -1,8 +0,0 @@
-import { Reducer } from 'redux';
-export declare class ReduxWorker {
-    reducerFn: Reducer;
-    constructor();
-    actionHandler: (evt: MessageEvent) => void;
-    addReducer: (reducerFn: Reducer<any, import("redux").AnyAction>) => void;
-    destroy: () => void;
-}
